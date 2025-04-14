e333123bc5027a9f615494aca8c825b982b680af
jiminseong <iamjms4237@gachon.ac.kr>
2025-04-14 19:31:19 +0900

    cors 대응

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
 app.use(express.json());
 
 app.post("/api/check", checkHandler);
